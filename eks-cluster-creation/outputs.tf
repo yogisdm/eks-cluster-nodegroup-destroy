ee92a27f3941c132a8c3cff99b9e0df0393b365f
unknown <yogisdmec007@gmail.com>
2023-04-12 15:41:30 +0530

    code update

--- eks-cluster-creation/outputs.tf
+++ eks-cluster-creation/outputs.tf
@@ -1,4 +1,4 @@
-output "eks_creation" {
-  description = "The ID of the VPC"
-  value       =  aws_eks_cluster.eks_sandbox_cluster.endpoint
-}
+//output "eks_creation" {
+//  description = "The ID of the VPC"
+//  value       =  aws_eks_cluster.eks_sandbox_cluster.endpoint
+//}
