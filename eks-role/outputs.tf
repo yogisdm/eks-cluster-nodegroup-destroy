8a4283ab1d5cc1750889a728e0abbd0cc48310bd
unknown <yogisdmec007@gmail.com>
2023-04-12 15:39:37 +0530

    code update

--- eks-role/outputs.tf
+++ eks-role/outputs.tf
@@ -1,7 +1,7 @@
 //output "eks_role" {
 //  description = "Role arn for eks cluster"
 //  value       = module.aws_iam_role.eks-iam-role.arn
-}
+//}
 
 //output "workernodes_role" {
 //  description = "Role arn for worker nodes"
